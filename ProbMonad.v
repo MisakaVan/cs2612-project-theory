e23b34d937e9d9b35347a017e038e33e6b55bb40
MisakaVan <2102315149@qq.com>
2025-01-18 05:07:07 +0800

    lemma: remove duplicate lemma

--- ProbMonad.v
+++ ProbMonad.v
@@ -1711,24 +1711,6 @@ Proof.
 Qed.
 
 
-#[export] Instance sum_congr :
-  Proper (Permutation (A:=R) ==> eq) (sum).
-Proof.
-  unfold Proper, respectful.
-  intros l1 l2 H.
-  induction H.
-  - reflexivity.
-  - simpl.
-    f_equal.
-    assumption.
-  - simpl.
-    rewrite Rplus_comm.
-    rewrite Rplus_assoc.
-    f_equal.
-    rewrite Rplus_comm.
-    reflexivity.
-  - transitivity (sum l'); assumption.
-Qed.
 
 Lemma perm_map:
   forall (A B: Type) (f: A -> B) (l1 l2: list A),
